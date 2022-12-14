b197419b353d771960fa43cde40f63b5742f4d74
SkylerAuraArena <skylerauraarena@gmail.com>
2022-12-14 18:17:08 +0100

    Fixing issue with the createdCardEventDisplayed on reloading after clicking the add card button

--- src/components/modal/createCardModal/CreateCardModal.tsx
+++ src/components/modal/createCardModal/CreateCardModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Backdrop from "../backdropModal/ModalBackdrop";
 import css from "../../elements/giftCard/giftCard.module.css";
@@ -12,17 +12,28 @@ type ModalProps = {
   handleClose : () => void,
 }
 
-const CreateCardModal: React.FC<ModalProps> = ({ handleClose }) => {
+const CreateCardModal: FC<ModalProps> = ({ handleClose }) => {
 
     const { dappContextState, createCard } = useDappContext()
+    const [trigger, setTrigger] = useState(false)
+    const [cardCreationSuccess, setCardCreationSuccess] = useState(false)
 
     //width: clamp(50%, 700px, 90%)
     const cardCss = `w-[800px] h-[825px] flex justify-start items-center flex-col gap-4 relative ${css.card} m-auto px-0 py-8 rounded-3xl text-gtCardLightBLue`
     const cardGTLogo = `w-24 absolute right-4 bottom-8 ${css.model}`;
 
     const fund = (newCardData: INewCardProps) => {
-      createCard(newCardData);      
+      createCard(newCardData);
+      setTrigger(true);
     }
+
+    useEffect(() => {
+      trigger && dappContextState.lastEvent?.name === "CardCreated" && setCardCreationSuccess(true);
+       return () => {
+        setCardCreationSuccess(false);
+      }
+    }, [dappContextState.lastEvent, trigger])
+    
 
     return (
       <Backdrop onClick={handleClose}>
@@ -37,19 +48,16 @@ const CreateCardModal: React.FC<ModalProps> = ({ handleClose }) => {
            <motion.button  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className='absolute w-12 p-3 bg-slate-500 top-5 right-5 text-white rounded-full cursor-pointer' onClick={handleClose}>X</motion.button>
            <h2 className='text-3xl'>Création d'une nouvelle carte</h2>
             {
-              !dappContextState.displayEvent && <>
+              !cardCreationSuccess && !dappContextState.displayEvent ? <>
                 <CreateCardForm func={fund} />
-              </>
-            }
-            {
-              dappContextState.displayEvent && <>
+              </> : <>
                 <div className='h-full flexJIC flex-col gap-12 text-3xl text-center'>
                   <h3>Nouvelle carte créée avec succès !</h3>
                   <div>Adresse de la carte : 
-                    <span className="text-white">{` ${dappContextState.lastEvent?.address}`}</span>
+                    <span className="text-white">{` ${dappContextState.currentCard?.address}`}</span>
                   </div>
                   <div>Somme versée à la création : 
-                    <span className="text-white">{` ${dappContextState.lastEvent?.amount} `}</span>
+                    <span className="text-white">{` ${dappContextState.currentCard?.coinsAmount} `}</span>
                     eth
                   </div>
                 </div>
